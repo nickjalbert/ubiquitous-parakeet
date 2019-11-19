a2ee5cda0afb6267e50012ea560fa91e957aa98e
Nick Jalbert <nickjalbert@gmail.com>
2019-11-19 17:02:39 -0500

    testing module imports etc

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import _ from 'lodash';
+import Cartpole from './cartpole';
 
 function component() {
+  const cartpole = new Cartpole('Test');
   const element = document.createElement('div');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack', cartpole.getName()], ' ');
 
   return element;
 }
