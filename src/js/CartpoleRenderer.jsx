f5b89d83285e152ea7f7bbf50247296601d90cc8
Nick Jalbert <nickjalbert@gmail.com>
2019-11-21 15:17:53 -0500

    Container for rendering

--- src/js/CartpoleRenderer.jsx
+++ src/js/CartpoleRenderer.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import Cartpole from './Cartpole';
-import styles from '../styles/index.module';
+import styles from '../styles/cartpole.module';
 
 function CartpoleRenderer() {
   const cartpole = useState(() => new Cartpole('ReactTest'))[0];
   return (
-    <div>
+    <div className={styles.cartpoleContainer}>
       <h1 className={styles.redText}>
         { cartpole.getName() }
       </h1>
