fa9533920db2ca8f2aba1f6fffa5da85542acc3d
Nick Jalbert <nickjalbert@gmail.com>
2019-11-28 00:16:08 -0500

    making progress

--- src/js/TFAgentPanel.jsx
+++ src/js/TFAgentPanel.jsx
@@ -4,39 +4,46 @@ import CartPoleEngine from './CartPoleEngine';
 
 import styles from '../styles/cartpole.module';
 
-const TRAINING_BATCHES = 20;
-const ROLLOUTS_PER_BATCH = 10;
+const TRAINING_EPOCHS = 6;
+const ROLLOUTS_PER_EPOCH = 10;
 const REWARD_DISCOUNT = 0.9;
 
 class Trainer {
   static doTraining() {
     const model = tf.sequential({
       layers: [
-        tf.layers.dense({ inputShape: [4], units: 4, activation: 'relu' }),
-        tf.layers.dense({ units: 4, activation: 'relu' }),
+        tf.layers.dense({ inputShape: [4], units: 16, activation: 'relu' }),
+        tf.layers.dense({ units: 8, activation: 'relu' }),
         tf.layers.dense({ units: 1, activation: 'sigmoid' }),
       ],
     });
-    model.compile({ optimizer: 'adam', loss: 'binaryCrossentropy' });
-    Trainer.doTrainingStep(model, 0);
+    model.compile({ optimizer: 'sgd', loss: 'binaryCrossentropy' });
+    Trainer.doTrainingEpochs(model, 0);
   }
 
-  static doTrainingStep(model, iteration) {
-    if (iteration >= TRAINING_BATCHES) {
-      console.log(`Training finished at iteration ${iteration}`);
+  static doTrainingEpochs(model, epochNum) {
+    if (epochNum >= TRAINING_EPOCHS) {
+      console.log(`Training finished at epochNum ${epochNum}`);
       return;
     }
-    console.log(`Getting rollouts for training iteration ${iteration}`);
-    const rolloutFn = () => Trainer.getRollout(model, true);
-    const rollouts = Array(ROLLOUTS_PER_BATCH).fill().map(rolloutFn);
-    console.log(`Training for training iteration ${iteration}`);
-    Trainer.trainModel(model, rollouts, iteration);
+    Trainer.evaluateModel(model, epochNum);
+    console.log(`Running epoch ${epochNum}`);
+    const rollouts = Trainer.getRollouts(ROLLOUTS_PER_EPOCH, model, true);
+    Trainer.trainModel(model, rollouts, epochNum);
+  }
 
-    const trainedRolloutFn = () => Trainer.getRollout(model, false);
-    const trainedRollouts = Array(ROLLOUTS_PER_BATCH).fill().map(trainedRolloutFn)
-    const actions = trainedRollouts.reduce((a, b) => a + b.length, 0);
-    const avgR = actions / trainedRollouts.length;
-    console.log(`Done training batch ${iteration}: ${avgR} avg reward`);
+  static evaluateModel(model, epochNum) {
+    const rollouts = Trainer.getRollouts(ROLLOUTS_PER_EPOCH, model, false);
+    console.log(rollouts);
+    const actions = rollouts.reduce((a, b) => a + b.length, 0);
+    const avgR = actions / rollouts.length;
+    console.log(`Rollouts from epoch ${epochNum}: ${avgR} avg reward`);
+  }
+
+  static getRollouts(count, model, explore) {
+    const getRolloutFn = () => Trainer.getRollout(model, explore);
+    const rollouts = Array(count).fill().map(getRolloutFn);
+    return Trainer.normalizeRewards(rollouts);
   }
 
   static getRollout(model, explore) {
@@ -44,69 +51,83 @@ class Trainer {
     const rollout = [];
     while (!state.done) {
       const stateTensor = Trainer.getTensorFromState(state);
-      const [
-        nnAction,
-        chosenAction,
-      ] = Trainer.getOnPolicyAction(model, stateTensor, explore);
-      rollout.push([stateTensor, nnAction, chosenAction]);
-      state = CartPoleEngine.step(chosenAction, state);
+      const action = Trainer.getOnPolicyAction(model, stateTensor, explore);
+      rollout.push([stateTensor, action]);
+      state = CartPoleEngine.step(action, state);
     }
-    return Trainer.calculateRewards(rollout);
+    return rollout;
   }
 
   static getOnPolicyAction(model, stateTensor, explore) {
     const predict = model.predict(stateTensor);
     const leftProbability = predict.arraySync()[0][0];
+    if (!explore ) {
+      //console.log(leftProbability);
+    }
     const nnAction = leftProbability > 0.5 ? 0 : 1;
     const chosenAction = Math.random() <= leftProbability ? 0 : 1;
-    return [nnAction, explore ? chosenAction : nnAction];
+    return explore ? chosenAction : nnAction;
   }
 
-  static calculateRewards(rollout) {
-    rollout.reverse();
-    let currReward = 0;
-    const enhancedRollout = [];
+  static normalizeRewards(rollouts) {
     const allRewards = [];
-    rollout.forEach(([state, nnAction, chosenAction]) => {
-      currReward = 1 + (REWARD_DISCOUNT * currReward);
-      enhancedRollout.push([state, nnAction, chosenAction, currReward]);
-      allRewards.push(currReward);
+    const rewardedRollouts = rollouts.map((rollout) => {
+      rollout.reverse();
+      let currReward = 0;
+      const rewardedRollout = rollout.map(([state, action]) => {
+        currReward = 1 + (REWARD_DISCOUNT * currReward);
+        allRewards.push(currReward);
+        return [state, action, currReward];
+      });
+      rewardedRollout.reverse()
+      return rewardedRollout;
     });
-    enhancedRollout.reverse();
 
     // normalize reward
     const moments = tf.moments(allRewards);
     const mean = moments.mean.arraySync();
     const stdDev = Math.sqrt(moments.variance.arraySync());
-    return enhancedRollout.map(([state, nnAction, chosenAction, reward]) => {
-      const normedReward = (reward - mean) / stdDev;
-      return [state, nnAction, chosenAction, normedReward];
+    return rewardedRollouts.map((rollout) => {
+      return rollout.map(([state, action, reward]) => {
+        const normedReward = (reward - mean) / stdDev;
+        return [state, action, normedReward];
+      });
     });
   }
 
-  static trainModel(model, rollouts, iteration) {
+  static trainModel(model, rollouts, epochNum) {
     const stateFn = (roll) => roll[0].arraySync()[0];
     const states = rollouts.map((rollout) => rollout.map(stateFn)).flat();
     const rewardFn = (roll) => {
-      const nnAction = roll[1];
-      const chosenAction = roll[2];
-      const reward = roll[3];
+      const action = roll[1];
+      const reward = roll[2];
+      if (reward > 0 && action === 0) {
+        return 1;
+      }
+      if (reward > 0 && action === 1) {
+        return 0;
+      }
+      if (reward <= 0 && action === 0) {
+        return 0;
+      }
+      if (reward <= 0 && action === 1) {
+        return 1;
+      }
+      /*
       if (reward > 0) {
-        if (nnAction === chosenAction) {
-          return nnAction;
-        }
-        return chosenAction;
+        // console.log(`returning good ${action} with reward ${reward}`);
+        return action;
       }
-      if (nnAction === chosenAction) {
-        return nnAction ? 0 : 1;
-      }
-      return nnAction;
-    };
+      const negatedAction = action ? 0 : 1;
+      // console.log(`returning ${negatedAction}, was ${action} with reward ${reward}`);
+      return negatedAction
+      */
+    }
     const rewards = rollouts.map((rollout) => rollout.map(rewardFn)).flat();
     const trainTensor = tf.tensor(states, [states.length, 4]);
     const trainResults = tf.tensor(rewards, [rewards.length, 1]);
     model.fit(trainTensor, trainResults).then(
-      () => Trainer.doTrainingStep(model, iteration + 1),
+      () => Trainer.doTrainingEpochs(model, epochNum + 1),
     );
   }
 
